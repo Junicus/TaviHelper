45a887700c42150dbf57bf865dc3641ed799ec24
Nelson Segarra <junicus@outlook.com>
2017-01-10 18:14:34 -0400

    added background task so it speaks, selecting output device and on a queue

--- TaviHelper/TaviHelper/Program.cs
+++ TaviHelper/TaviHelper/Program.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using System.Speech;
 using Meebey.SmartIrc4net;
 using System.Speech.Synthesis;
+using System.IO;
+using NAudio.Wave;
+using System.Threading;
+using Newtonsoft.Json;
 
 namespace TaviHelper
 {
@@ -13,15 +17,45 @@ namespace TaviHelper
     {
         public const string Server = "irc.chat.twitch.tv";
         public const int Port = 6667;
+        public static BotInfo botinfo = new BotInfo()
+        {
+            Nickname = "LastRobot",
+            Password = ""
+        };
+
         public const string Channel = "#tavinnea";
-        public const string Nickname = "LastRobot";
-        public const string Password = "";
         private static IrcClient ircClient = new IrcClient();
-        public static SpeechSynthesizer reader = new SpeechSynthesizer();
+        public static Queue<byte[]> _speakQueue = new Queue<byte[]>();
+        private static SpeakTaskManager _speakTaskManager;
+        public static int g_DeviceNumber = 0;
 
         static void Main(string[] args)
         {
-            
+            if (!File.Exists("botinfo.json"))
+            {
+                Console.WriteLine("Using default botsettings");
+                var botinfoString = JsonConvert.SerializeObject(botinfo);
+                File.WriteAllText("botinfo.json", botinfoString);
+            }
+            else
+            {
+                var botinfostring = File.ReadAllText("botinfo.json");
+                botinfo = JsonConvert.DeserializeObject<BotInfo>(botinfostring);
+            }
+
+            var waveOutCount = WaveOut.DeviceCount;
+            Console.WriteLine($"There are {waveOutCount} devices");
+            for (var i = 0; i < waveOutCount; i++)
+            {
+                Console.WriteLine($"Device {i} = {WaveOut.GetCapabilities(i).ProductName}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Select device output: ");
+            var device = Console.ReadLine();
+
+            _speakTaskManager = new SpeakTaskManager(_speakQueue, g_DeviceNumber);
+            _speakTaskManager.Start();
+
             ircClient.OnConnected += IrcClient_OnConnected;
             ircClient.OnWriteLine += IrcClient_OnWriteLine;
             ircClient.OnReadLine += IrcClient_OnReadLine;
@@ -32,23 +66,21 @@ namespace TaviHelper
 
         private static void IrcClient_OnChannelMessage(object sender, IrcEventArgs e)
         {
-            if(e.Data.Channel == Channel)
+            if (e.Data.Channel == Channel)
             {
-                if (e.Data.Nick != "tavinnea" && e.Data.Nick != "lastrobot")
+                if (e.Data.Nick != "tavinnea" && e.Data.Nick != botinfo.Nickname.ToLower())
                 {
-                    reader.Speak(e.Data.Message);
+                    var data = Say($"{e.Data.Nick} said {e.Data.Message}");
+                    _speakQueue.Enqueue(data);
                 }
             }
         }
 
         private static void IrcClient_OnConnected(object sender, EventArgs e)
         {
-            ircClient.Login(Nickname, Nickname, 0, "", Password);
-            //ircClient.RfcNick(Nickname);
-            //ircClient.RfcPass(Password);
+            ircClient.Login(botinfo.Nickname, botinfo.Nickname, 0, "", botinfo.Password);
             ircClient.WriteLine("CAP REQ :twitch.tv/membership");
             ircClient.RfcJoin(Channel);
-            ircClient.RfcPrivmsg(Channel, "Testing");
             ircClient.Listen();
         }
 
@@ -64,7 +96,14 @@ namespace TaviHelper
         {
             var currColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($" < {e.Line}");
+            if (e.Line.StartsWith("PASS"))
+            {
+                Console.WriteLine($" < PASS ***");
+            }
+            else
+            {
+                Console.WriteLine($" < {e.Line}");
+            }
             Console.ForegroundColor = currColor;
         }
 
@@ -74,6 +113,75 @@ namespace TaviHelper
             Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine($"ERROR : {e.Data.RawMessage}");
             Console.ForegroundColor = currColor;
+        }
+
+        public static byte[] Say(string text)
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var synth = new SpeechSynthesizer())
+                {
+                    synth.SetOutputToWaveStream(stream);
+                    synth.Speak(text);
+                }
+                return stream.ToArray();
+            }
+        }
+
+        private class SpeakTaskManager
+        {
+            private CancellationTokenSource _cancelSource = new CancellationTokenSource();
+            private int _playbackDevice = 0;
+
+            public SpeakTaskManager(Queue<byte[]> speakQueue, int playbackDevice)
+            {
+                _playbackDevice = playbackDevice;
+            }
+
+            public void Start()
+            {
+                var cancelToken = _cancelSource.Token;
+                Task.Factory.StartNew(() => speakPump(cancelToken), cancelToken);
+            }
+
+            public void Stop()
+            {
+                _cancelSource.Cancel();
+            }
+
+            private void speakPump(CancellationToken cancelToken)
+            {
+                try
+                {
+                    while (!cancelToken.IsCancellationRequested)
+                    {
+                        if (_speakQueue.Any())
+                        {
+                            var data = _speakQueue.Dequeue();
+                            PlayData(data);
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+
+            public void PlayData(byte[] data)
+            {
+                var waveOut = new WaveOut();
+                var stream = new MemoryStream(data);
+
+                var reader = new WaveFileReader(stream);
+                waveOut.DeviceNumber = _playbackDevice;
+                waveOut.Init(reader);
+                waveOut.Play();
+                while (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    Task.Delay(100);
+                }
+            }
         }
     }
 }
